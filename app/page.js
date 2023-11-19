0809461c1fb5fd80d81a218fa5b6139ea1bfb76d
Thanh Pham <khanhthy@yahoo.com>
2023-11-19 15:06:26 -0800

    Uppdated index page with some styling.

--- app/page.js
+++ app/page.js
@@ -1,7 +1,39 @@
+import Link from "next/link"
+import Image from "next/image"
+
 export default function Home() {
   return (
     <main>
-      <h2>Dashboard.</h2>
+      <section id="intro">
+        <div className="container-lg">
+          <div className="grid md:grid-cols-2 md:gap-3">
+            <div className="w-full text-center md:text-start">
+              <h1>
+                <div className="text-8xl font-bold my-10 text-center">
+                   <p>Le Phan.</p>
+                </div>
+                <div className="text-4xl font-semibold text-center">
+                   <p>Art Galerry.</p>
+                </div>
+              </h1>
+              <p className="text-center">
+                This is a paragraph.
+              </p>
+              <div className="text-center">
+                <Link href="/contact" className="btn-primary">Get Contact</Link>
+              </div>
+            </div>
+            <div className="text-center hidden md:block rounded border border-spacing-1 border-green-300">
+              <Image
+                  alt="Image Alt"
+                  src="/le-phan.jpg"
+                  width={900}
+                  height={700}
+              />
+            </div>
+          </div>
+        </div>
+      </section>
     </main>
   )
 }
