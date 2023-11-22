789b7c01cf7af2c4225d30475f9195063ca4c1af
Thanh Pham <khanhthy@yahoo.com>
2023-11-21 23:14:40 -0800

    Added Photo NotFound page

---
+++ app/photos/[id]/not-found.jsx
@@ -0,0 +1,12 @@
+import Link from "next/link"
+export default function notFound() {
+  return (
+   <main className="text-center">
+        <h2 className="text-3xl">
+            We hit the brick here.
+        </h2>
+        <p>We could not find the photo you were looking for.</p>
+        <p>Go back to all <Link href="/photos">Photos</Link></p>
+   </main>
+  )
+}
