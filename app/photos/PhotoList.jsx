382b8d91fae661571adae0a7f4de7a1d2615d936
Thanh Pham <khanhthy@yahoo.com>
2023-11-21 22:06:29 -0800

    Added Link into PhotoList page to  link to PhotoDetail page

--- app/photos/PhotoList.jsx
+++ app/photos/PhotoList.jsx
@@ -1,4 +1,5 @@
 import Image from "next/image"
+import Link from "next/link"
 
 async function getPhotos(){
     const res = await fetch('http://localhost:4000/images', {
@@ -17,19 +18,20 @@ export default async function PhotoList() {
       <div className="grid grid-cols-3 gap-1">
       {photos?.map((photo)=>{ 
         return (
-          <div className="card my-5">
-            <div key={photo.id} className="">
-              <Image
-                    alt={photo.pho}
-                    src={`${photo.pho}`}
-                    width={300}
-                    height={340}
-                />
-              <h2 className="text-center">{photo.name}</h2>
-              <h3 className="text-center">{photo.author}</h3>
+          <Link href={`/photos/${photo.id}`} key={photo.id}>
+            <div className="card my-5">
+              <div className="">
+                <Image
+                      alt={photo.pho}
+                      src={`${photo.pho}`}
+                      width={300}
+                      height={340}
+                  />
+                <h2 className="text-center">{photo.name}</h2>
+                <h3 className="text-center">{photo.author}</h3>
+              </div>
             </div>
-            
-          </div>
+          </Link>
         ) 
       })}
       </div>
