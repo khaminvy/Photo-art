eb1d4fbbcbf76ec62e13190385a2be0a222a953b
Thanh Pham <khanhthy@yahoo.com>
2023-11-24 17:15:24 -0800

    Added a link to homepage and style link active state

--- app/components/navbar.jsx
+++ app/components/navbar.jsx
@@ -1,14 +1,18 @@
+"use client"
 import Link from "next/link";
+import { usePathname } from "next/navigation"
 
 export default function Navbar() {
+  const pathname = usePathname()
   return (
     <nav>
         <h2>Le Phan Art Gallery</h2>
         <div>
-            <Link href="/about">About</Link>
-            <Link href="/contact">Contact</Link>
-            <Link href="/photos">Photos</Link>
-            <Link href="/create">Create</Link>
+            <Link href="/" className={`link ${pathname === '/' ? 'active' : ''}`}>Home</Link>
+            <Link href="/about" className={`link ${pathname === '/about' ? 'active' : ''}`}>About</Link>
+            <Link href="/contact" className={`link ${pathname === '/contact' ? 'active' : ''}`}>Contact</Link>
+            <Link href="/photos" className={`link ${pathname === '/photos' ? 'active' : ''}`}>Photos</Link>
+            <Link href="/create" className={`link ${pathname === '/create' ? 'active' : ''}`}>Create</Link>
         </div>
     </nav>
   )
