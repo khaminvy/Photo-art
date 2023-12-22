21f066ee2d3987226959b15c757886ca6fafd38e
Thanh Pham <khanhthy@yahoo.com>
2023-12-21 21:47:30 -0800

    add style for mobile

--- app/components/navbar.jsx
+++ app/components/navbar.jsx
@@ -7,24 +7,31 @@ export default function Navbar() {
   const pathname = usePathname()
   return (
     <nav>
-        <div className="flex justify-start mx-3">
-          <Image
-            alt="Image Alt"
-            src="/img/my-girl.jpg"
-            width={50}
-            height={50}
-            priority
-            className="rounded-full"
-          />
-          <h2>Le Phan Art Gallery</h2>
+        <div className="flex justify-between mx-2 w-full">
+          <div className="flex justify-start mx-2 items-center">
+            <Image
+              alt="Image Alt"
+              src="/img/my-girl.jpg"
+              width={50}
+              height={50}
+              priority
+              className="rounded-full"
+            />
+            <h2 className="orange_gradient mx-2">Le Phan Art Gallery</h2>
+          </div>
+          <div class="px-4 cursor-pointer text-slate-950 block md:hidden" id="burger">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 5.25h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5" />
+            </svg>                          
+          </div>
         </div>
-       
-        <div>
-            <Link href="/" className={`link ${pathname === '/' ? 'active' : ''}`}>Home</Link>
-            <Link href="/about" className={`link ${pathname === '/about' ? 'active' : ''}`}>About</Link>
-            <Link href="/contact" className={`link ${pathname === '/contact' ? 'active' : ''}`}>Contact</Link>
-            <Link href="/photos" className={`link ${pathname === '/photos' ? 'active' : ''}`}>Photos</Link>
-            <Link href="/create" className={`link ${pathname === '/create' ? 'active' : ''}`}>Create</Link>
+
+        <div className="">
+            <Link href="/" className={`link ${pathname === '/' ? 'active' : ''} hidden md:block`}>Home</Link>
+            <Link href="/about" className={`link ${pathname === '/about' ? 'active' : ''} hidden md:block`}>About</Link>
+            <Link href="/contact" className={`link ${pathname === '/contact' ? 'active' : ''} hidden md:block`}>Contact</Link>
+            <Link href="/photos" className={`link ${pathname === '/photos' ? 'active' : ''} hidden md:block`}>Photos</Link>
+            <Link href="/create" className={`link ${pathname === '/create' ? 'active' : ''} hidden md:block`}>Create</Link>
         </div>
     </nav>
   )
--- app/page.js
+++ app/page.js
@@ -10,10 +10,10 @@ export default function Home() {
           <div className="grid md:grid-cols-2 md:gap-3">
             <div className="w-full text-center md:text-start">
               <h1>
-                <div className="text-8xl font-bold my-10 text-center drop-shadow-lg">
+                <div className="text-8xl font-bold my-10 text-center drop-shadow-lg orange_gradient">
                    <p className="">Le Phan.</p>
                 </div>
-                <div className="text-4xl font-semibold text-center">
+                <div className="text-4xl font-semibold text-center blue_gradient">
                    <p>Art Galerry.</p>
                 </div>
               </h1>
@@ -21,7 +21,7 @@ export default function Home() {
                 Lorem, ipsum dolor sit amet consectetur adipisicing elit.
               </p>
               <div className="text-center py-2">
-                <Link href="/contact" className="btn-primary px-4 py-3 rounded">Get Contact</Link>
+                <Link href="/contact" className="btn-primary px-4 py-2 rounded">Get Contact</Link>
               </div>
             </div>
             <div className="text-center hidden md:block 
