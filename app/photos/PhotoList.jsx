b2116f29c484e66d111797181f4dc6227275d342
Thanh Pham <khanhthy@yahoo.com>
2023-12-26 22:16:56 -0800

    Update photo list

--- app/photos/PhotoList.jsx
+++ app/photos/PhotoList.jsx
@@ -6,7 +6,7 @@ async function getPhotos(){
  
 
   //initate delay
-  //await new Promise(resolve => setTimeout(resolve, 4000))
+  await new Promise(resolve => setTimeout(resolve, 4000))
   try {
     const apiUrl = process.env.NEXT_PUBLIC_API_URL
     const res = await fetch(`${apiUrl}/api/photos`, {
