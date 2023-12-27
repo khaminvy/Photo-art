85d99fb8611a4ceab9ee54fcda02a8775e4610c1
Thanh Pham <khanhthy@yahoo.com>
2023-12-26 21:54:52 -0800

    updated navbar

--- app/components/navbar.jsx
+++ app/components/navbar.jsx
@@ -12,7 +12,7 @@ export default function Navbar() {
 
 
   return (
-    <nav>
+    <nav className="">
         <div className="flex justify-between mx-2 w-full">
           <div className="flex justify-start mx-2 items-center">
             <Image
@@ -25,7 +25,7 @@ export default function Navbar() {
             />
             <h2 className="orange_gradient mx-2">Le Phan Art Gallery</h2>
           </div>
-          <div className="px-4 cursor-pointer text-slate-950 block md:hidden relative bg-inherit text-sm dropdown">
+          <div className="px-4 cursor-pointer text-slate-900 block md:hidden relative bg-inherit text-sm dropdown">
             <button onClick={openMenu} className="flex flex-col justify-end items-center">
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
                   <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 5.25h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5" />
@@ -46,7 +46,7 @@ export default function Navbar() {
           </div>
         </div>
 
-        <div className="" onClick={openMenu}>
+        <div className="blue_gradient" onClick={openMenu}>
             <Link href="/" className={`link ${pathname === '/' ? 'active' : ''} hidden md:block`}>Home</Link>
             <Link href="/about" className={`link ${pathname === '/about' ? 'active' : ''} hidden md:block`}>About</Link>
             <Link href="/contact" className={`link ${pathname === '/contact' ? 'active' : ''} hidden md:block`}>Contact</Link>
